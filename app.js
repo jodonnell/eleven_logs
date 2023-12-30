59717541bf60e22095aea4c7c82ed2ab4962bf93
Jacob O'Donnell <jacobodonnell@gmail.com>
2023-12-29 22:14:33 -0500

    printing data on html

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 import { parseFiles } from './parser.js'
 
+const template = (sessions) => (
+`
+<div>
+service fault percentage: ${sessions.serviceFaultPercentage}
+</div>
+<div>
+win on service percentage: ${sessions.serviceAcePercentage}
+</div>
+<div>
+win on service return percentage: ${sessions.serviceReturnAcePercentage}
+</div>
+<div>
+win point on serve percentage: ${sessions.winServePercentage}
+</div>
+`
+)
+
 const logsUpload = document.getElementById('logs-upload')
 logsUpload.onchange = function () {
   const file = logsUpload.files[0]
@@ -9,15 +26,10 @@ logsUpload.onchange = function () {
   reader.onload = function (evt) {
     const contents = evt.target.result
     const sessions = parseFiles(file.name, contents)
-    console.log('service fault percentage: ', sessions.serviceFaultPercentage)
-    document.getElementById("page").innerHTML = `service fault percentage: ${sessions.serviceFaultPercentage}`
+
+    document.getElementById("page").innerHTML = template(sessions)
   }
   reader.onerror = function (evt) {
     document.getElementById("page").innerHTML = "error reading file";
   }
 }
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  const page = document.getElementById('page')
-  page.innerHTML = 'cow'
-})
